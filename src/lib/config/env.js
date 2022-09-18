47c29da2423e0feb95498e9828b1de6dafda06a0
MdZillurRahman <mzisanr@gmail.com>
2022-09-18 12:35:45 +0600

    Ready to deploy

--- src/lib/config/env.js
+++ src/lib/config/env.js
@@ -1,3 +1,2 @@
 import 'dotenv/config'
-export const HTTP_ENDPOINT =
-  process.env.HTTP_ENDPOINT || 'http://localhost:7000'
+export const HTTP_ENDPOINT = process.env.HTTP_ENDPOINT || 'http://localhost:7000'
